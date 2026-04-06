63691d1b140b86a6ed2ec337ce15c260a39704df
CJ Chen <sijia1996@gmail.com>
2026-04-06 15:04:29 -0700

    Remove banner

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import AprilFools from './components/AprilFools'
 import Home from './components/Home'
 import Blogs from './components/Blogs'
 import Blog from './components/Blog'
+import Resume from './components/Resume'
 import Footer from './components/Footer'
 import avatar from './res/avatar-grey.png'
 import jellyfish from './res/jellyfish.svg'
-import construction from './res/construction.png'
 import {
   BrowserRouter as Router,
   Routes,
@@ -39,11 +39,11 @@ function App() {
             </li>
           </ul>
         </nav>
-        <img src={construction} alt="construction" style={{ "maxWidth": "300px" }} />
         {/* <div className="section center-text"> <p>Welcome to my blog!</p> <p>I write about Test Automation and my experience as a female engineer and immigrant. My posts are entirely my own and do not reflect the views of my employer or anyone else.</p>
         </div> */}
         <Routes>
           <Route exact path="/homepage/" element={<Home />} />
+          <Route path="/homepage/resume" element={<Resume />} />
           <Route path="/homepage/blogs" element={<Blogs />} />
           <Route path="/homepage/blog/:id" element={<Blog />} />
           <Route path="/homepage/blog-posts/1" element={<AprilFools />} />
---
+++ src/components/Resume.js
@@ -0,0 +1,475 @@
+export default function Resume() {
+  return (
+    <div className="resume">
+      <div class="center">
+        <p>
+          <strong>Sijia (CJ) Chen</strong>
+          <br />
+          (415) 988-0835 <span class="math inline">|</span>{" "}
+          <a href="mailto:sijia1996@gmail.com">
+            <u>sijia1996@gmail.com</u>
+          </a>{" "}
+          <span class="math inline">|</span>{" "}
+          <a href="https://www.linkedin.com/in/cj-chen/">
+            <u>linkedin.com/in/cj-chen</u>
+          </a>
+          <span class="math inline">|</span>{" "}
+          <a href="https://github.com/cjschen">
+            <u>github.com/cjschen</u>
+          </a>{" "}
+          <span class="math inline">|</span> San Francisco, CA
+        </p>
+      </div>
+      <div class="multicols">
+        <p>
+          <span>2</span>
+        </p>
+        <h1 id="about-me">About Me</h1>
+        <p>
+          University of Waterloo educated Platform Engineer with 8 years of
+          professional experience who is passionate about building robust and
+          scalable tooling and backend platforms.
+        </p>
+        <h1 id="skills">Skills</h1>
+        <p>
+          Python, C#, Serverless, CI/CD, ECS, Javascript, Datadog, Postgres,
+          RDS, Jenkins, Bash, React, Kubernetes, AWS Lambda, Splunk,
+          LaunchDarkly, Terraform
+        </p>
+      </div>
+      <h1 id="experience">Experience</h1>
+      <ul>
+        <li>
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <strong>Autodesk Inc</strong>
+                </td>
+                <td>San Francisco, CA</td>
+              </tr>
+            </tbody>
+          </table>
+        </li>
+        <li>
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <strong>Senior Software Engineer</strong>
+                </td>
+                <td>
+                  <em>Oct 2022 – Apr 2026</em>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <ul>
+            <li>
+              <p>
+                <span>
+                  {" "}
+                  <span>
+                    <strong>Led and onboarded new team</strong> focused on
+                    platformizing quality tooling and test-data generation{" "}
+                  </span>
+                </span>
+              </p>
+            </li>
+            <li>
+              <p>
+                <span>
+                  {" "}
+                  <span>
+                    <strong>
+                      Designed, developed, and maintained a serverless lambda
+                      service
+                    </strong>{" "}
+                    using C# and Python, supporting validation of data in new
+                    datacenters and load testing across distributed systems,
+                    reducing test data creation time from{" "}
+                    <strong>2 months to 30 minutes</strong>.{" "}
+                  </span>{" "}
+                </span>
+              </p>
+            </li>
+            <li>
+              <p>
+                <span>
+                  {" "}
+                  <span>
+                    Collaborated with multiple engineering teams across Autodesk
+                    to promote <strong>inner sourcing</strong>, enabling
+                    cross-team adoption and shared ownership.{" "}
+                  </span>{" "}
+                </span>
+              </p>
+            </li>
+            <li>
+              <p>
+                <span>
+                  {" "}
+                  <span>
+                    <strong>Executed large-scale data migration</strong>
+                    for a 24x large Amazon RDS instance with{" "}
+                    <strong>8k QPS</strong> to enable read replicas, failovers
+                    and improve performance.{" "}
+                  </span>
+                </span>
+              </p>
+            </li>
+            <li>
+              <p>
+                <span>
+                  {" "}
+                  <span>
+                    <strong>Led expansion</strong> of Autodesk Construction
+                    Cloud components into new Australian Datacenter to
+                    facilitate performance improvements and data sovereignty
+                    requirements for customers.{" "}
+                  </span>{" "}
+                </span>
+              </p>
+            </li>
+            <li>table
+              <p>
+                <span>
+                  {" "}
+                  <span>
+                    <strong>
+                      Led Construction Backend Engineering Town Hall
+                    </strong>
+                    , facilitating cross-team collaboration on shared backend
+                    architecture and platform challenges.{" "}
+                  </span>{" "}
+                </span>
+              </p>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <strong>Software Engineer</strong>
+                </td>
+                <td>
+                  <em>Aug 2019 – Oct 2022</em>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <ul>
+            <li>
+              <p>
+                <span>
+                  {" "}
+                  <span>
+                    Developed and maintained CI/CD pipelines and reusable test
+                    packages to enable feature teams to build robust automated
+                    testing frameworks and improve test quality and
+                    coverage.{" "}
+                  </span>
+                </span>
+              </p>
+            </li>
+            <li>
+              <p>
+                <span>
+                  {" "}
+                  <span>
+                    <strong>Maintained and scaled</strong> an internal service
+                    on Kubernetes responsible for generating, persisting, and
+                    dynamically assigning complex project data to users on
+                    demand for validation workflows.{" "}
+                  </span>{" "}
+                </span>
+              </p>
+            </li>
+            <li>
+              <p>
+                <span>
+                  {" "}
+                  <span>
+                    Led monthly meetings and events for Female and Non-Binary
+                    R&amp;D members within Autodesk Construction Solutions,
+                    fostering inclusion, mentorship, and cross-team
+                    collaboration.{" "}
+                  </span>
+                </span>
+              </p>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <h1 id="education">Education</h1>
+      <ul>
+        <li>
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <strong>University of Waterloo</strong>
+                </td>
+                <td>
+                  <span>Ontario, Canada</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <em>Bachelor Degree in Honours Computer Science</em>
+                </td>
+                <td>
+                  <em>Sep 2014 – Jun 2019</em>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </li>
+      </ul>
+      <div class="multicols*">
+        <p>
+          <span>2</span>
+        </p>
+        <h1 id="internships">Internships</h1>
+        <ul>
+          <li>
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <strong>SDET Intern</strong>
+                  </td>
+                  <td>San Francisco, CA</td>
+                </tr>
+                <tr>
+                  <td>PlanGrid (acq. Autodesk)</td>
+                  <td>
+                    <em>Sept 2018 – Apr 2019</em>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <ul>
+              <li>
+                <p>
+                  <span>
+                    {" "}
+                    <span>Kotlin, Python </span>{" "}
+                  </span>
+                </p>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <strong>Quality Automation Intern</strong>
+                  </td>
+                  <td>San Francisco, CA</td>
+                </tr>
+                <tr>
+                  <td>
+                    FutureAdvisor (acq. BlackRock)
+                  </td>
+                  <td>
+                    <em>Jan 2018 – Apr 2018</em>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <ul>
+              <li>
+                <p>
+                  <span>
+                    {" "}
+                    <span>
+                      Reduced test run time from 3 hours to 30 min using
+                      Selenium Element Caching{" "}
+                    </span>{" "}
+                  </span>
+                </p>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <strong>Co-op Student</strong>
+                  </td>
+                  <td>Toronto, Canada</td>
+                </tr>
+                <tr>
+                  <td>Toronto Transit Commission</td>
+                  <td>
+                    <em>May 2017 – Aug 2017</em>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </li>
+          <li>
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <strong>Security Data Analyst</strong>
+                  </td>
+                  <td>Toronto, Canada</td>
+                </tr>
+                <tr>
+                  <td>Royal Bank of Canada</td>
+                  <td>
+                    <em>Sept 2016 – Dec 2016</em>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </li>
+          <li>
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <strong>Mobile Developer</strong>
+                  </td>
+                  <td>Toronto, Canada</td>
+                </tr>
+                <tr>
+                  <td>New Signature</td>
+                  <td>
+                    <em>Jan 2016 – Apr 2016</em>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </li>
+          <li>
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <strong>Developer</strong>
+                  </td>
+                  <td>Toronto, Canada</td>
+                </tr>
+                <tr>
+                  <td>Evermight</td>
+                  <td>
+                    <em>May 2015 – Aug 2015</em>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </li>
+        </ul>
+        <h1 id="projects">Projects</h1>
+        <ul>
+          <li>
+            <p>
+              <span>
+                <a href="https://github.com/cjschen/google-projects/">
+                  <u>
+                    <strong>Financial Reporter</strong>
+                  </u>
+                </a>{" "}
+                <span class="math inline">|</span>
+                <em>Kubernetes, Google Cloud API</em>
+              </span>
+            </p>
+            <ul>
+              <li>
+                <p>
+                  <span>
+                    {" "}
+                    <span>
+                      Fetches transaction reports from Gmail, processes,
+                      categorizes and pushes it into Google Sheets. Run as a
+                      Kubernetes CronJob on home server{" "}
+                    </span>{" "}
+                  </span>
+                </p>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <p>
+              <span>
+                <a href="https://github.com/cjschen/homepage">
+                  <u>
+                    <strong>Personal Website</strong>
+                  </u>
+                </a>{" "}
+                <span class="math inline">|</span>{" "}
+                <em>React, Javascript, Github Actions</em>
+              </span>
+            </p>
+            <ul>
+              <li>
+                <p>
+                  <span>
+                    {" "}
+                    <span>
+                      Portfolio Website originally deployed on Heroku, then
+                      Github Pages. Deployed using Github Actions{" "}
+                    </span>
+                  </span>
+                </p>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <p>
+              <span>
+                <a href="https://github.com/cjschen/ao3-stats">
+                  <u>
+                    <strong>AO3 Trends</strong>
+                  </u>
+                </a>{" "}
+                <span class="math inline">|</span>
+                <em>BeautifulSoup, SQLite, Matplotlib</em>
+              </span>
+            </p>
+            <ul>
+              <li>
+                <p>
+                  <span>
+                    {" "}
+                    <span>
+                      Web Scrapes archiveofourown.org in accordance with the
+                      robot.txt file requested rate limit{" "}
+                    </span>{" "}
+                  </span>
+                </p>
+              </li>
+              <li>
+                <p>
+                  <span>
+                    {" "}
+                    <span>
+                      Processes tags associated with fan works using NumPy and
+                      displays trends in fandoms across time{" "}
+                    </span>{" "}
+                  </span>
+                </p>
+              </li>
+              <li>
+                <p>
+                  <span>
+                    {" "}
+                    <span>Data Storage with SQLite DB and SQLAlchemy </span>
+                  </span>
+                </p>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
